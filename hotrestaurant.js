bd6b8f2c85126c60905f141799c18c85058ad44c
Sam Estorga <estorgajr@gmail.com>
2019-08-08 19:54:07 -0700

    added eventpreventdefault

--- hotrestaurant.js
+++ hotrestaurant.js
@@ -1,22 +1,24 @@
-$(".submit").on("click", function(event) {
-$.post("/api/tables", newReservation, function(data) {
-    if (data) {
-        alert("You've been booked!");
-    } else {
-        alert("Sorry were pretty busy!");
-    };
+$(".submit").on("click", function (event) {
+    event.preventDefault();
+    
+    $.post("/api/tables", newReservation, function (data) {
+        if (data) {
+            alert("You've been booked!");
+        } else {
+            alert("Sorry were pretty busy!");
+        };
 
-    $("reserve-id").val("");
-    $("reserve-name").val("");
-    $("reserve-email").val("");
-    $("reserve-phone").val("");
-})
+        $("reserve-id").val("");
+        $("reserve-name").val("");
+        $("reserve-email").val("");
+        $("reserve-phone").val("");
+    })
 
-let newReservation = {
-    customerID: $("#reserve-id").val.trim,
-    customerName: $("#reserve-name").val.trim,
-    customerEmail: $("#reserve-email").val.trim,
-    customerPhone: $("#reserve-phone").val.trim
-};
-})
+    let newReservation = {
+        customerID: $("#reserve-id").val.trim,
+        customerName: $("#reserve-name").val.trim,
+        customerEmail: $("#reserve-email").val.trim,
+        customerPhone: $("#reserve-phone").val.trim
+    }
+});
 
